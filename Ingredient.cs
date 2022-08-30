fa2e6087b07b9423c67ca485454ee3f389496fa9
JervinAlejandro <jervin.alejandro@hotmail.com>
2022-08-31 00:36:38 +0800

    -Fixed Design Foundation

---
+++ Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheeBogGrocery
+{
+    internal class Ingredient
+    {
+        public string name { get; set; }
+        public double weight { get; set; }
+        public int cost { get; set; }
+        public string location { get; set; }
+        public string metric { get; set; }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace CheeBogGrocery
         {
             InitializeComponent();
         }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void header_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void populateData()
+        {
+            List<Dish> dishes = new List<Dish>();
+            for (int i = 0; i < 8; i++)
+            {
+                Dish dish = new Dish();
+                dishes.Add(dish);
+            }
+
+            dishes[0].name = "CHICKEN POPCORN";
+            dishes[1].name = "CHICKEN SKEWER";
+            dishes[2].name = "GRAHAM CAKE";
+            dishes[3].name = "HONEY CHICKEN BAO";
+            dishes[4].name = "HONEY CHICKEN POP";
+            dishes[5].name = "PORK BAO";
+            dishes[6].name = "PORK SKEWER";
+            dishes[7].name = "PRAWN NACHOS";
+        }
+
+        private List<Ingredient> setIngredients(Dish dish,int counter)
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+            for(int i = 0; i < counter; i++)
+            {
+                Ingredient ingredient = new Ingredient();
+            }
+
+            return ingredients;
+        }
     }
 }
